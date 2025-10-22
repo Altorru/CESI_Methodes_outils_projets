eb57c9502527ce923f204da770484b538d58b6fc
Hugo VIAUD <hugoviaud85@gmail.com>
2025-10-22 10:23:57 +0200

    Add singleton java example

---
+++ POO/Test.java
@@ -0,0 +1,8 @@
+package POO;
+
+public class Test {
+    public static int attribut1 = 5;
+    public static void main(String[] args) {
+        System.out.println("Attribut1: " + attribut1);
+    }
+}
