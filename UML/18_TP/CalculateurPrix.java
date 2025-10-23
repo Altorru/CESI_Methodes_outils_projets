b79e3b296f0fcd3a25add2fb65f129db4d18e9e5
Hugo VIAUD <hugoviaud85@gmail.com>
2025-10-23 09:19:44 +0200

    Refactor pricing calculation to use strategy pattern, removing old method and updating main to demonstrate new implementation

--- UML/18_TP/CalculateurPrix.java
+++ UML/18_TP/CalculateurPrix.java
@@ -1,11 +1,17 @@
 public class CalculateurPrix {
-    public double calculerPrix(double prix, String typeClient) {
-        if (typeClient.equals("ETUDIANT")) {
-            return prix * 0.8;
-        } else if (typeClient.equals("VIP")) {
-            return prix * 0.7;
-        } else {
-            return prix;
-        }
+    // ----- OLD METHOD -----
+    // public double calculerPrix(double prix, String typeClient) {
+    //     if (typeClient.equals("ETUDIANT")) {
+    //         return prix * 0.8;
+    //     } else if (typeClient.equals("VIP")) {
+    //         return prix * 0.7;
+    //     } else {
+    //         return prix;
+    //     }
+    // }
+    // ----------------------
+
+    public double calculerPrix(double prix, StrategieReduction strategie) {
+        return strategie.appliquerReduction(prix);
     }
 }
--- UML/18_TP/Main.java
+++ UML/18_TP/Main.java
@@ -7,10 +7,8 @@ public class Main {
         // ------------------
 
         // --- NEW METHOD USING STRATEGY PATTERN ---
-        StrategieReduction etudiantStrategy = new StudentStrategy();
-        StrategieReduction vipStrategy = new VipStrategy();
-        double prixInitial = 100;
-        System.out.println("Prix étudiant : " + etudiantStrategy.appliquerReduction(prixInitial));
-        System.out.println("Prix VIP : " + vipStrategy.appliquerReduction(prixInitial));
+        CalculateurPrix calculateur = new CalculateurPrix();
+        System.out.println("Prix étudiant : " + calculateur.calculerPrix(100, new StudentStrategy()));
+        System.out.println("Prix VIP : " + calculateur.calculerPrix(100, new VipStrategy()));
     }
 }
